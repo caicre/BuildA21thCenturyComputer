591b47ccdfbb5d282a2fa668b4551042cd6a67e0
Rainatam <rainatam9784@gmail.com>
2017-11-24 22:03:49 +0800

    modified cpu and alu

--- cpu.vhd
+++ cpu.vhd
@@ -1,7 +1,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity cpu is 
+entity cpu is
 	port(
 		rst			: in STD_LOGIC;
 		clk			: in STD_LOGIC;
@@ -26,39 +26,37 @@ entity cpu is
 		Ram2WE		: out STD_LOGIC;
 		Ram2EN		: out STD_LOGIC;
 		Ram2Addr	: out STD_LOGIC_VECTOR(17 downto 0);
-		Ram2Data	: inout STD_LOGIC_VECTOR(15 downto 0);
+		Ram2Data	: inout STD_LOGIC_VECTOR(15 downto 0)
 
 		-- Flash
 	);
-end cpu
+end cpu;
 
 architecture behavioral of cpu is
 
-	component Controller is
+	component Controller
 		port(
-			rst 				: in STD_LOGIC;
-			instruction			: in STD_LOGIC_VECTOR(15 downto 0);
-			controllerSignal 	: out STD_LOGIC_VECTOR
+			rst 		: in STD_LOGIC;
+			inst		: in STD_LOGIC_VECTOR(15 downto 0);
+			controlSignal: out STD_LOGIC_VECTOR(31 downto 0)
 		);
 	end component;
 
-	component ForwardingUnit is
+	component ForwardingUnit
 		port(
 			-- control signal
 			EX_RegDst	: in STD_LOGIC_VECTOR(3 downto 0);
 			MEM_RegDst	: in STD_LOGIC_VECTOR(3 downto 0);
-			EX_RegWrite	: in STD_LOGIC;
-			MEM_RegWrite: in STD_LOGIC;
 			-- input
 			raddr1 		: in STD_LOGIC_VECTOR(3 downto 0);
 			raddr2 		: in STD_LOGIC_VECTOR(3 downto 0);
 			-- output
 			ForwardA	: out STD_LOGIC_VECTOR(1 downto 0);
-			ForwardB 	: out STD_LOGIC_VECTOR(1 downto 0);
+			ForwardB 	: out STD_LOGIC_VECTOR(1 downto 0)
 		);
 	end component;
 
-	component HazardDetectionUnit is
+	component HazardDetectionUnit
 		port(
 			-- control signal
 			EX_MemRead 	: in STD_LOGIC;
@@ -73,9 +71,19 @@ architecture behavioral of cpu is
 		);
 	end component;
 
+	component 
 	----------------------------
 	--          IF            
 	----------------------------
+	component PCRegister
+		port(
+			clk 		: in STD_LOGIC;
+			rst 		: in STD_LOGIC;
+			PCIn 		: in STD_LOGIC_VECTOR(15 downto 0);
+			PCOut		: out STD_LOGIC_VECTOR(15 downto 0);
+		);
+	end component;
+
 	component PCAdder
 		port(
 			PCIn		: in STD_LOGIC_VECTOR(15 downto 0);
@@ -83,14 +91,14 @@ architecture behavioral of cpu is
 		);
 	end component;
 
-	component RPCAdder is
+	component RPCAdder
 		port(
 			PCIn		: in STD_LOGIC_VECTOR(15 downto 0);
 			PCOut		: out STD_LOGIC_VECTOR(15 downto 0)
 		);
 	end component;
 
-	component PCMux is
+	component PCMux
 		port(
 			-- control signal
 			jump		: in STD_LOGIC;
@@ -107,7 +115,7 @@ architecture behavioral of cpu is
 		);
 	end component;
 
-	component InstructionMemory is
+	component InstructionMemory
 		port(
 			clk 		: in STD_LOGIC;
 			rst 		: in STD_LOGIC;
@@ -124,7 +132,7 @@ architecture behavioral of cpu is
 		);
 	end component;
 
-	component IFIDRegister is
+	component IFIDRegister
 		port(
 			rst 		: in STD_LOGIC;
 			clk 		: in STD_LOGIC;
@@ -141,7 +149,7 @@ architecture behavioral of cpu is
 	----------------------------
 	--          ID            
 	----------------------------
-	component Registers is
+	component Registers
 		port(
 			clk 		: in STD_LOGIC;
 			rst 		: in STD_LOGIC;
@@ -158,19 +166,19 @@ architecture behavioral of cpu is
 		);
 	end component;
 
-	component ImmUnit is
+	component ImmUnit
 		port(
 			-- control signal
 			ImmSrc		: in STD_LOGIC_VECTOR(2 downto 0);
 			ExtendOp	: in STD_LOGIC;
 			--
-			instruction : in STD_LOGIC_VECTOR(15 downto 0);
+			inst 		: in STD_LOGIC_VECTOR(15 downto 0);
 			--
 			immOut		: out STD_LOGIC_VECTOR(15 downto 0)
 		);
 	end component;
 
-	component IDEXRegister is
+	component IDEXRegister
 		port(
 			clk 		: in STD_LOGIC;
 			rst 		: in STD_LOGIC;
@@ -193,8 +201,7 @@ architecture behavioral of cpu is
 			ID_raddr1	: in STD_LOGIC_VECTOR(3 downto 0);
 			ID_raddr2	: in STD_LOGIC_VECTOR(3 downto 0);
 			ID_imm		: in STD_LOGIC_VECTOR(15 downto 0);
-			ID_RPC 		: in STD_LOGIC_VECTOR(15 downto 0);
-			ID＿waddr	: in STD_LOGIC_VECTOR(3 downto 0);
+			ID_RPC 		: in STD_LOGIC_VECTOR(15 downto 0)
 			-- output control signal
 			EX_RegDst	: out STD_LOGIC_VECTOR(3 downto 0);
 			EX_ALUOp	: out STD_LOGIC_VECTOR(3 downto 0);
@@ -214,15 +221,13 @@ architecture behavioral of cpu is
 			EX_raddr1	: out STD_LOGIC_VECTOR(3 downto 0);
 			EX_raddr2	: out STD_LOGIC_VECTOR(3 downto 0);
 			EX_imm		: out STD_LOGIC_VECTOR(15 downto 0);
-			EX_RPC 		: out STD_LOGIC_VECTOR(15 downto 0);
-			EX＿waddr	: out STD_LOGIC_VECTOR(3 downto 0);
-			EX＿waddr	: out STD_LOGIC_VECTOR(3 downto 0)
+			EX_RPC 		: out STD_LOGIC_VECTOR(15 downto 0)
 		);
 	end component;
 	----------------------------
 	--          EX            
 	----------------------------
-	component ALUSrcMux1 is
+	component ALUSrcMux1
 		port(
 			-- control signal
 			ForwardA	: in STD_LOGIC_VECTOR(1 downto 0);
@@ -235,7 +240,7 @@ architecture behavioral of cpu is
 		);
 	end component;
 
-	component ALUSrcMux2 is
+	component ALUSrcMux2
 		port(
 			-- control signal
 			ForwardB	: in STD_LOGIC_VECTOR(1 downto 0);
@@ -250,19 +255,18 @@ architecture behavioral of cpu is
 		);
 	end component;
 
-	component ALU is
+	component ALU
 		port(
 			-- input
 			src1		: in STD_LOGIC_VECTOR(15 downto 0);
 			src2		: in STD_LOGIC_VECTOR(15 downto 0);
 			ALUOp		: in STD_LOGIC_VECTOR(3 downto 0);
 			-- output
-			result		: out STD_LOGIC_VECTOR(15 downto 0);
-			zero		: out STD_LOGIC
+			result		: out STD_LOGIC_VECTOR(15 downto 0)
 		);
 	end component;
 
-	component ALUResMux is
+	component ALUResMux
 		port(
 			-- control signal
 			ALURes 		: in STD_LOGIC_VECTOR(1 downto 0);
@@ -275,7 +279,7 @@ architecture behavioral of cpu is
 		);
 	end component;
 
-	component PCImmAdder is
+	component PCImmAdder
 		port(
 			PCIn		: in STD_LOGIC_VECTOR(15 downto 0);
 			imm 		: in STD_LOGIC_VECTOR(15 downto 0);
@@ -283,11 +287,27 @@ architecture behavioral of cpu is
 		);
 	end component;
 
-	component EXMEMRegister is
+	component BranchMux
+		port(
+			-- control signal
+			ForwardA	: in STD_LOGIC_VECTOR(1 downto 0);
+			Branch 		: in STD_LOGIC;
+			BranchOp 	: in STD_LOGIC_VECTOR(1 downto 0);
+			-- input
+			reg1 		: in STD_LOGIC_VECTOR(15 downto 0);
+			EX_ALURes 	: in STD_LOGIC_VECTOR(15 downto 0);
+			MEM_ALURes 	: in STD_LOGIC_VECTOR(15 downto 0);
+			-- output
+			BranchJudge : out STD_LOGIC
+		);
+	end component;
+
+	component EXMEMRegister
 		port(
 			clk 		: in STD_LOGIC;
 			rst 		: in STD_LOGIC;
 			-- input control signal
+			EX_RegDst 	: in STD_LOGIC_VECTOR(3 downto 0);
 			EX_BranchOp	: in STD_LOGIC_VECTOR(1 downto 0);
 			EX_Branch 	: in STD_LOGIC;
 			EX_MemRead	: in STD_LOGIC;
@@ -296,9 +316,9 @@ architecture behavioral of cpu is
 			EX_RegWrite : in STD_LOGIC;
 			-- input
 			EX_ALURes	: in STD_LOGIC_VECTOR(15 downto 0);
-			EX_reg2		: in STD_LOGIC_VECTOR(15 downto 0);
-			EX_waddr	: in STD_LOGIC_VECTOR(3 downto 0);
+			EX_reg2		: in STD_LOGIC_VECTOR(15 downto 0)
 			-- output control signal
+			MEM_RegDst 	: out STD_LOGIC_VECTOR(3 downto 0);
 			MEM_BranchOp: out STD_LOGIC_VECTOR(1 downto 0);
 			MEM_Branch 	: out STD_LOGIC;
 			MEM_MemRead	: out STD_LOGIC;
@@ -307,26 +327,14 @@ architecture behavioral of cpu is
 			MEM_RegWrite: out STD_LOGIC;
 			-- output
 			MEM_ALURes	: out STD_LOGIC_VECTOR(15 downto 0);
-			MEM_reg2	: out STD_LOGIC_VECTOR(15 downto 0);
-			MEM_waddr	: out STD_LOGIC_VECTOR(3 downto 0)
+			MEM_reg2	: out STD_LOGIC_VECTOR(15 downto 0)
 		);
 	end component;
 	----------------------------
 	--          MEM            
 	----------------------------
-	component BranchMux is
-		port(
-			-- control signal
-			Branch 		: in STD_LOGIC;
-			BranchOp 	: in STD_LOGIC_VECTOR(1 downto 0);
-			-- input
-			ALUZero		: in STD_LOGIC;
-			-- output
-			BranchJudge : out STD_LOGIC
-		);
-	end component;
 
-	component DataMemory is 
+	component DataMemory 
 		port(
 			clk 		: in STD_LOGIC;
 			rst 		: in STD_LOGIC;
@@ -347,30 +355,30 @@ architecture behavioral of cpu is
 		);
 	end component;
 
-	component MEMWBRegister is 
+	component MEMWBRegister 
 		port(
 			clk 		: in STD_LOGIC;
 			rst 		: in STD_LOGIC;
 			-- input control signal
+			MEM_RegDst 	: in STD_LOGIC_VECTOR(3 downto 0);
 			MEM_MemToRead: in STD_LOGIC;
 			MEM_RegWrite: in STD_LOGIC;
 			-- input
 			MEM_rdata 	: in STD_LOGIC_VECTOR(15 downto 0);
-			MEM_ALURes	: in STD_LOGIC_VECTOR(15 downto 0);
-			MEM_waddr	: in STD_LOGIC_VECTOR(3 downto 0);
+			MEM_ALURes	: in STD_LOGIC_VECTOR(15 downto 0)
 			-- output control signal
+			WB_RegDst 	: out STD_LOGIC_VECTOR(3 downto 0);
 			WB_MemToRead: out STD_LOGIC;
 			WB_RegWrite : out STD_LOGIC;
 			-- output
 			WB_rdata 	: out STD_LOGIC_VECTOR(15 downto 0);
 			WB_ALURes	: out STD_LOGIC_VECTOR(15 downto 0);
-			WB_waddr	: out STD_LOGIC_VECTOR(3 downto 0)
 		);
 	end component;
 	----------------------------
 	--          WB            
 	----------------------------
-	component WriteDataMux is
+	component WriteDataMux
 		port(
 			-- control signal
 			MemToReg: in STD_LOGIC;
@@ -381,3 +389,72 @@ architecture behavioral of cpu is
 			wdata 		: out STD_LOGIC_VECTOR(15 downto 0);
 		);
 	end component;
+
+	----------------------------
+	--         signals            
+	----------------------------
+	-- PCRegister
+	signal PC 	 		: STD_LOGIC_VECTOR(15 downto 0);
+	_
+	-- PCAdder
+	signal PCAddOne 	: STD_LOGIC_VECTOR(15 downto 0);
+
+	-- RPCAdder
+	signal PCAddTwo 	: STD_LOGIC_VECTOR(15 downto 0);
+
+	-- PCMux
+	signal PCOut 		: STD_LOGIC_VECTOR(15 downto 0);
+
+	-- InstructionMemory
+	signal IR 			: STD_LOGIC_VECTOR(15 downto 0);
+
+	-- IFIDRegister
+	signal ID_PC 		: STD_LOGIC_VECTOR(15 downto 0);
+	signal ID_IR		: STD_LOGIC_VECTOR(15 downto 0);
+	signal ID_RPC		: STD_LOGIC_VECTOR(15 downto 0);
+
+	-- Registers
+	signal reg1 		: STD_LOGIC_VECTOR(15 downto 0);
+	signal reg2 		: STD_LOGIC_VECTOR(15 downto 0);
+
+	-- ImmUnit
+	signal immOut 		: STD_LOGIC_VECTOR(15 downto 0);
+
+	-- IDEXRegister
+	signal EX_RegDst	: STD_LOGIC_VECTOR(3 downto 0);
+	signal EX_ALUOp 	: STD_LOGIC_VECTOR(3 downto 0);
+	signal EX_ALUSrcB	: STD_LOGIC;
+	signal EX_ALURes 	: STD_LOGIC;
+	signal EX_Jump 		: STD_LOGIC;
+	signal EX_BranchOp	: STD_LOGIC_VECTOR(1 downto 0);
+	signal EX_Branch 	: STD_LOGIC;
+	signal EX_MemRead 	: STD_LOGIC;
+	signal EX_MemWrite	: STD_LOGIC;
+	signal EX_MemToRead : STD_LOGIC;
+	signal EX_RegWrite  : STD_LOGIC;
+	signal EX_PC 		: STD_LOGIC_VECTOR(15 downto 0);
+	signal EX_reg1 		: STD_LOGIC_VECTOR(15 downto 0);
+	signal EX_reg2 		: STD_LOGIC_VECTOR(15 downto 0);
+	signal EX_raddr1 	: STD_LOGIC_VECTOR(3 downto 0);
+	signal EX_raddr2	: STD_LOGIC_VECTOR(3 downto 0);
+	signal EX_imm 		: STD_LOGIC_VECTOR(15 downto 0);
+	signal EX_RPC 		: STD_LOGIC_VECTOR(3 downto 0);
+
+	-- ALUSrcMux1
+	signal ALUSrc1 		: STD_LOGIC_VECTOR(15 downto 0);
+
+	-- ALUSrcMux2
+	signal ALUSrc2 		: STD_LOGIC_VECTOR(15 downto 0);
+
+	-- ALU
+	signal result	 	: STD_LOGIC_VECTOR(15 downto 0);
+	signal zero 		: STD_LOGIC;
+
+	-- ALUResMux
+	signal ALUResult	: STD_LOGIC_VECTOR(15 downto 0);
+
+	-- PCImmAdder
+	signal PCAddImm		: STD_LOGIC_VECTOR(15 downto 0);
+
+	-- EXMEMRegister
+	signal MEM_BranchOp	: STD_
