66fd45639de9463de22b3cded0d3d60f6642f7eb
李睿燮 <liruixie15@mails.tsinghua.edu.cn>
2017-12-03 16:35:37 +0800

    memoryUnit

--- MemoryUnit.vhd
+++ MemoryUnit.vhd
@@ -35,13 +35,9 @@ entity MemoryUnit is
 	port(
 			clk 		: in STD_LOGIC;
 			rst 		: in STD_LOGIC;
-			
-			
 			-- input control signal
 			MemWrite : in STD_LOGIC;		--'1':写
 			MemRead 	: in STD_LOGIC;		--'1':读
-			IsMem		: in STD_LOGIC;		--'0': 只有IF操作 '1': 只有MEM操作(IF,ID,EX要停顿)
-			
 			-- RAM1								--为串口(BF00~BF03)
 			Ram1_OE 		: out STD_LOGIC;
 			Ram1_WE 		: out STD_LOGIC;
@@ -71,7 +67,21 @@ entity MemoryUnit is
 			tsre			: in STD_LOGIC;
 			wrn			: out STD_LOGIC;
 			rdn			: out STD_LOGIC;
-	);
+			
+			--FLASH								--监控程序
+			FLASH_ADDR 	: out STD_LOGIC_VECTOR(22 downto 0);
+			FLASH_DATA	: inout STD_LOGIC_VECTOR(15 downto 0);
+			FLASH_BYTE	: out STD_LOGIC := '1';		--flash操作模式, 常置'1'
+			FLASH_VPEN	: out STD_LOGIC := '1';		--flash写保护, 常置'1'
+			FLASH_RP		: out STD_LOGIC := '1';		--'1'表示flash工作, 常置'1'
+			FLASH_CE		: out STD_LOGIC := '0';		--flash使能
+			FLASH_OE		: out STD_LOGIC := '1';		--flash读使能, '0'有效, 每次都操作后值'1'
+			FLASH_WE		: out STD_LOGIC := '1';		--flash写使能
+			
+			--output
+			FLASH_FINISH: out STD_LOGIC := '0'		--'0':未完成	'1':完成读监控程序到RAM2
+																--这要转给控制器, 要把PC?IF?停顿
+				);
 end MemoryUnit;
 
 architecture Behavioral of MemoryUnit is
@@ -79,9 +89,6 @@ architecture Behavioral of MemoryUnit is
 type state is (s0, s1, s2, s3, s4, s5);
 signal mem_state : state;
 signal rflag : std_logic := '0';		--rflag='1'代表把串口数据线（ram1_data）置高阻，用于节省状态的控制
-
-shared variable Ram_Addr : std_logic_vector(15 downto 0) := (others => '0');	--随IF或MEM而改变的输入信号
-shared variable Ram_Data : std_logic_vector(15 downto 0) := (others => '0');	--随IF或MEM而改变的输入信号
 
 --flash
 signal flash_state : state;
@@ -109,10 +116,16 @@ begin
 			inst <= (others => '0');
 			
 			mem_state <= s0;
+			flash_state <= s0;
 			
 			current_addr <= (others => '0');
 			
 		elsif (clk'event and clk = '1') then
+			if(flash_finished = '1') then
+				FLASH_BYTE <= '1';
+				FLASH_VPEN <= '1';
+				FLASH_RP <= '1';
+				FLASH_CE <= '1';
 				Ram1_EN <= '1';
 				Ram1_OE <= '1';
 				Ram1_WE <= '1';
@@ -215,7 +228,71 @@ begin
 					
 				end if; --RAM2写/读
 			else
+				if( cnt >= 1000) then
+					cnt := 0;
+					
+					case flash_state is
+						when s0 =>		--WE置0
+							Ram2_EN <= '0';
+							Ram2_WE <= '0';
+							Ram2_OE <= '1';
+							wrn <= '1';
+							rdn <= '1';
+							FLASH_WE <= '0';
+							FLASH_OE <= '1';
+							
+							FLASH_BYTE <= '1';
+							FLASH_VPEN <= '1';
+							FLASH_RP <= '1';
+							FLASH_CE <= '0';
+							
+							flash_state <= s1;
+							
+						when s1 =>		--置为读模式
+							FLASH_DATA <= x"00FF";
+							flash_state <= s2;
+							
+						when s2 =>
+							FLASH_WE <= '1';
+							flash_state <= s3;
+							
+						when s3 =>		--准备读取
+							FLASH_ADDR <= "000000" & current_addr & '0';	--这是由于字模式中只用到22~1, 其中最高6位(22~17)为block地址, 之后(16~1)为block内的地址.
+																			--我们是只用第一个block
+							FLASH_DATA <= (others => 'Z');
+							FLASH_OE <= '0';
+							flash_state <= s4;
+							
+						when s4 =>		--读取完毕, 准备写入到内存
+							FLASH_OE <= '1';	
+							Ram2_WE <= '0';
+							Ram2_Addr <= "00" & current_addr;
+							--Ram2_AddrOutput <= "00" & current_addr;	--调试
+							Ram2_Data <= FLASH_DATA;
+							flash_state <= s5;
+						
+						when s5 =>		--写入到内存
+							Ram2_WE <= '1';
+							current_addr <= current_addr + '1';
+							flash_state <= s0;
+						
+							
+						when others =>
+							flash_state <= s0;
+					end case;
+					
+					if (current_addr > x"0249") then		--这个地址随时kernel.bin的长度而改变??
+						flash_finished <= '1';
+					end if;
+				else
+					if (cnt < 1000) then
+						cnt := cnt + 1;
+					end if;
+				end if;	--cnt
+			end if; --flash_finished
 		end if;	--rst and clk's raise
 	end process;
+	
+	FLASH_FINISH <= flash_finished;
 	
 end Behavioral;
